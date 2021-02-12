b570778f80255204d0b814e235f41ed509a21c89
ryme07 <remydelrieu@hotmail.fr>
2021-02-12 18:40:09 +0100

    feat: createRenderer

--- src/js/createRenderer.js
+++ src/js/createRenderer.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 
 export default function createRenderer() {
-  const canvas = document.getElementById("app");
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  const canvas = document.getElementById("world");
+  const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-
+  renderer.shadowMap.enabled = true;
   canvas.appendChild(renderer.domElement);
   return renderer;
 }
