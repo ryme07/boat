165e5b9440690dfbbf39bf3654135c9147a79610
ryme07 <remydelrieu@hotmail.fr>
2021-02-12 18:39:30 +0100

    feat: create shadowlight and light

--- src/js/createLight.js
+++ src/js/createLight.js
@@ -1,7 +1,26 @@
 import * as THREE from "three";
 
-export default function createLight() {
-  const light = new THREE.PointLight(0xffffff, 1, 1000);
-  light.position.set(0, 0, 10);
-  return light;
+function hemisphereLight() {
+  const HemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, 0.9);
+  // light.position.set(0, 0, 10);
+  return HemisphereLight;
 }
+
+function shadowLight() {
+  const shadowLight = new THREE.DirectionalLight(0xffffff, 0.9);
+
+  shadowLight.position.set(150, 350, 350);
+  shadowLight.castShadow = true;
+  shadowLight.shadow.camera.left = -400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = -400;
+  shadowLight.shadow.camera.near = 1;
+  shadowLight.shadow.camera.far = 1000;
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+
+  return shadowLight;
+}
+
+export default { hemisphereLight, shadowLight };
