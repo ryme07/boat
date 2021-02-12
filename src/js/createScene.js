f6101bfc4c8dba009c5de5830b5b94e602131b38
ryme07 <remydelrieu@hotmail.fr>
2021-02-12 18:37:57 +0100

    feat: createScene

--- src/js/createScene.js
+++ src/js/createScene.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 
 export default function createScene() {
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xffffff);
+  // scene.background = new THREE.Color(0xffffff);
+  scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
   return scene;
 }
