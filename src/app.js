40bc25507ec2db0931e5eea0d42770feb669c0c6
ryme07 <remydelrieu@hotmail.fr>
2021-02-15 13:37:27 +0100

    feat: add sky

--- src/app.js
+++ src/app.js
@@ -5,19 +5,24 @@ import createRenderer from "./js/createRenderer";
 import createCamera from "./js/createCamera";
 import createScene from "./js/createScene";
 import createSea from "./js/createSea";
+import createSky from "./js/createSky";
 
 const renderer = createRenderer();
 const scene = createScene();
 const camera = createCamera();
 const sea = createSea();
+const sky = createSky();
 const hemisphereLght = createLight.hemisphereLight();
 const shadowLght = createLight.shadowLight();
 
 scene.add(hemisphereLght);
 scene.add(shadowLght);
 scene.add(sea);
+scene.add(sky);
 
 animate(() => {
+  sea.rotation.z += 0.001;
+  sky.rotation.z += 0.001;
   renderer.render(scene, camera);
 });
 
